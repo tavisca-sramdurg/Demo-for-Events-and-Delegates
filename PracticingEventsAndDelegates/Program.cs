bb157cd57d147241359ebe5672a2ca4bb33807d7
sramdurg <sramdurg@RENTLAPT0050>
2019-08-28 23:16:46 +0530

    Shifted the practice to main and made an invocation list

--- PracticingEventsAndDelegates/Program.cs
+++ PracticingEventsAndDelegates/Program.cs
@@ -4,11 +4,43 @@ namespace PracticingEventsAndDelegates
 {
     class Program
     {
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
+        public delegate void WorkCompletedHandler();
         static void Main(string[] args)
         {
-            Worker worker = new Worker();
-            worker.DoWork();
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed);
 
+            del1 += del2;
+            del1 += del3;
+
+            DoWork(del1);
+            //DoWork(del2);
         }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(500, WorkType.Coding);
+        }
+
+        private static void WorkPerformed(int hours, WorkType workType)
+        {
+            Console.WriteLine($"Work performed for {hours} hours and of type {workType}");
+        }
+
+        private static void WorkCompleted()
+        {
+            Console.WriteLine("WorkDone!");
+        }
+
+ 
+    }
+
+    public enum WorkType
+    {
+        Coding,
+        Analysis,
+        Documentation
     }
 }
--- PracticingEventsAndDelegates/Worker.cs
+++ PracticingEventsAndDelegates/Worker.cs
@@ -7,32 +7,11 @@ namespace PracticingEventsAndDelegates
     
     public class Worker
     {
-        public delegate void WorkPerformedHandler(int hours, WorkType workType);
-        WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed);
-
-        public delegate void WorkCompletedHandler();
-        WorkCompletedHandler del2 = new WorkCompletedHandler(WorkCompleted);
-
-        public void DoWork()
+        public void DoWork(int hours, WorkType workType)
         {
-            del1(100, WorkType.Coding);
-            del2();
-        }
 
-        private static void WorkPerformed(int hours, WorkType workType)
-        {
-            Console.WriteLine($"Work performed for {hours} hours and of type {workType}");
+            
         }
-        private static void WorkCompleted()
-        {
-            Console.WriteLine("WorkDone");
-        }
-
-        public enum WorkType
-        {
-            Coding,
-            Analysis,
-            Documentation
-        }
+       
     }
 }
